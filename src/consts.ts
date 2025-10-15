873eb04f2faec55ffd42ee75f62e1653c5606703
g_komissarov <g_komissarov@tockl.ru>
2025-10-15 10:01:01 +0300

    updated

--- src/consts.ts
+++ src/consts.ts
@@ -5,8 +5,8 @@ config();
 
 export const TOKEN = process.env.TOKEN_KEY;
 
-export const WEDNESDAY_HOUR = 9;
-export const WEDNESDAY_MINUTE = 3;
+export const WEDNESDAY_HOUR = 6;
+export const WEDNESDAY_MINUTE = 0;
 export const TIMEZONE = 'Europe/Moscow';
 export const POLL_QUESTION = `Игра ${dayjs().format('DD.MM')} в 21:00, Коломяги`;
 export const POLL_OPTIONS = ['Буду', 'Буду +1', 'Нет', 'Пока думаю'];
--- src/utils.ts
+++ src/utils.ts
@@ -2,16 +2,16 @@ import schedule, { Job, RecurrenceRule } from 'node-schedule';
 import { POLL_OPTIONS, POLL_QUESTION, TIMEZONE, WEDNESDAY_HOUR, WEDNESDAY_MINUTE } from './consts';
 import { Bot } from 'grammy';
 
-const logMessage = `Bot will send a poll every Wednesday in ${WEDNESDAY_HOUR}:${String(
-  WEDNESDAY_MINUTE
-).padStart(2, '0')} (${TIMEZONE}).`;
-
 export const makeRule = (): RecurrenceRule => {
   const rule = new schedule.RecurrenceRule();
-  rule.dayOfWeek = 2;
+  rule.dayOfWeek = 3;
   rule.tz = TIMEZONE;
   rule.hour = WEDNESDAY_HOUR;
   rule.minute = WEDNESDAY_MINUTE;
+
+  console.info(
+    `📅 Rule created: dayOfWeek=${rule.dayOfWeek}, hour=${rule.hour}, minute=${rule.minute}, tz=${rule.tz}`
+  );
   return rule;
 };
 
@@ -34,5 +34,8 @@ export const scheduleWeeklyPoll = async (
 
   scheduledJobs.set(chatId, job);
   console.info(`⏰ Job scheduled for chat ${chatId}.`);
+
+  const logMessage = `Bot will send a poll every Wednesday in ${WEDNESDAY_HOUR}:${WEDNESDAY_MINUTE}
+   (${TIMEZONE}).`;
   console.info(`📊 ${logMessage}`);
 };
